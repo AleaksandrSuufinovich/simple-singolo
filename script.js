6f9ae5d2bbed8d18053abbeb831e2f46816fc61b
Alexcooler <alexcooler060296@gmail.com>
2020-12-03 18:09:32 +0300

    docs:make a task

---
+++ script.js
@@ -0,0 +1,24 @@
+window.onload = function() {
+var list = document.querySelector(".portfolio_img"),
+button = document.querySelector(".filter");
+function shuffle(items) {
+    var cached = items.slice(0), temp, i = cached.length, rand;
+    while(--i) {
+        rand = Math.floor(i * Math.random());
+        temp = cached[rand];
+        cached[rand] = cached[i];
+        cached[i] = temp;
+    }
+    return cached;
+}
+function shuffleNodes() {
+    var nodes = list.children, i = 0;
+    nodes = Array.prototype.slice.call(nodes);
+    nodes = shuffle(nodes);
+    while(i < nodes.length) {
+        list.appendChild(nodes[i]);
+        ++i;
+    }
+}
+button.onclick = shuffleNodes;
+}
